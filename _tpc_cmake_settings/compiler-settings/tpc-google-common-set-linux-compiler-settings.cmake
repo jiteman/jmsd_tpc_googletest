ea9e959e2be59e3b226d88b4cf897c9dea1204b3
jiteman <jiteman@gmail.com>
2019-10-26 18:18:01 +0300

    tpc_google_mock has been added tpc_google_test has been disable due to its including into "unusual" way to build nested project, temporary

--- _tpc_cmake_settings/compiler-settings/tpc-google-common-set-linux-compiler-settings.cmake
+++ _tpc_cmake_settings/compiler-settings/tpc-google-common-set-linux-compiler-settings.cmake
@@ -1,17 +1,16 @@
 JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common-set-linux-compiler-settings.cmake" )
 
 if ( UNIX )
-	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+	set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
 
 	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-Wno-" ) #
 
-	##string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
+	## string( REPLACE ";" " " ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS}" )
 
 	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
+	string( REPLACE "-Wundef" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) # "" is not defined, evaluates to 0
 
-	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
-
-	## set( CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-format" )
+	set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
 else()
 	message( SEND_ERROR "[JMSD] ${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common COMPILER SETTINGS: ${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common-set-linux-compiler-settings.cmake is included while not on linux" )
 
